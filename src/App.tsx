96f4db23f069be554f9737ae95f11222e82449a8
scKim <lansco1229@gmail.com>
2025-06-26 19:28:19 +0900

    Add LargeWasteDisposalPage and FreeDisposalPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import './App.css';
 import React, { Suspense } from 'react';
 import LargeWasteDisposalPage from './pages/LargeWasteDisposalPage/LargeWasteDisposalPage';
+import FreeDisposalPage from './pages/FreeDisposalPage/FreeDisposalPage';
 
 const AppLayout = React.lazy(() => import('./layout/AppLayout'));
 
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route path="largewaste" element={<LargeWasteDisposalPage />} />
+          <Route path="freedisposal" element={<FreeDisposalPage />} />
         </Route>     
       </Routes>
     </Suspense>
