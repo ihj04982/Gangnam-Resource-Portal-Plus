698dfa5416a86dfd7ce8b8b84aa1e0996d2c9f9c
Heejeong Lim <ihj04982@gmail.com>
2025-06-26 18:58:34 +0900

    변경된 인터페이스명 적용용

--- src/apis/locationApi.ts
+++ src/apis/locationApi.ts
@@ -5,13 +5,13 @@ import type {
   ClothingCollectionLocation,
   CigaretteButtLocation,
   FluorescentBatteryLocation,
-  ILocationApiResponse,
+  LocationApiResponse,
 } from '../models/locations';
 
 export const getFluorescentBattery = async (
   page = 1,
   perPage = 10,
-): Promise<ILocationApiResponse<FluorescentBatteryLocation>> => {
+): Promise<LocationApiResponse<FluorescentBatteryLocation>> => {
   const response = await publicDataApi.get('/15038090/v1/uddi:254381dc-ecce-413b-a222-d70a2fceb2cd', {
     params: {
       page,
@@ -22,7 +22,7 @@ export const getFluorescentBattery = async (
   return response.data;
 };
 
-export const getWasteBag = async (page = 1, perPage = 10): Promise<ILocationApiResponse<WasteBagLocation>> => {
+export const getWasteBag = async (page = 1, perPage = 10): Promise<LocationApiResponse<WasteBagLocation>> => {
   const response = await publicDataApi.get('/15113123/v1/uddi:2fc8d9bc-08f6-4b0d-90b2-94d4a834e82d', {
     params: {
       page,
@@ -33,10 +33,7 @@ export const getWasteBag = async (page = 1, perPage = 10): Promise<ILocationApiR
   return response.data;
 };
 
-export const getCigaretteButt = async (
-  page = 1,
-  perPage = 10,
-): Promise<ILocationApiResponse<CigaretteButtLocation>> => {
+export const getCigaretteButt = async (page = 1, perPage = 10): Promise<LocationApiResponse<CigaretteButtLocation>> => {
   const response = await publicDataApi.get('/15103349/v1/uddi:06daef13-9cbe-463d-b7b4-b80b1b3ab815', {
     params: {
       page,
@@ -50,7 +47,7 @@ export const getCigaretteButt = async (
 export const getClothingCollection = async (
   page = 1,
   perPage = 10,
-): Promise<ILocationApiResponse<ClothingCollectionLocation>> => {
+): Promise<LocationApiResponse<ClothingCollectionLocation>> => {
   const response = await publicDataApi.get('/15127131/v1/uddi:a9873b46-9551-407a-aff5-a3a77befb3d4', {
     params: {
       page,
