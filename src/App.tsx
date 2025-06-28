98c5ecde4922bfc06c6ecbf5e90a1e340a491ce0
networkSocerer <gigattfyhig@gmail.com>
2025-06-28 11:39:27 +0900

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ import FaqDetail from './pages/NoticePage/FaqDetail';
 import AnnouncementsDetail from './pages/NoticePage/AnnouncementsDetail';
 import FreeDisposalPage from './pages/FreeDisposalPage/FreeDisposalPage';
 import CleanMainPage from './pages/CleanPage/CleanMainPage';
+import CleanMain from './pages/CleanPage/CleanMain';
+import Trash from './pages/CleanPage/component/Trash';
+import Sewage from './pages/CleanPage/component/Sewage';
+import RecycleStatistics from './pages/CleanPage/component/RecycleStatistics';
+import Recycle from './pages/CleanPage/component/Recycle';
+import Pet from './pages/CleanPage/component/Pet';
+import GNRecycleCenter from './pages/CleanPage/component/GNRecycleCenter';
+import GNEnvReCenter from './pages/CleanPage/component/GNEnvReCenter';
+import GeneralRequest from './pages/CleanPage/component/GeneralRequest';
+import BizTrash from './pages/CleanPage/component/BizTrash';
+import CoffeeGround from './pages/CleanPage/component/CoffeeGround';
+import GenCivilComplaint from './pages/CleanPage/component/GenCivilComplaint';
 
 const AppLayout = React.lazy(() => import('./layout/AppLayout'));
 const LocationPage = React.lazy(() => import('./pages/LocationPage/LocationPage'));
@@ -35,7 +47,20 @@ const App = () => {
           </Route>
 
           <Route path="locations" element={<LocationPage />} />
-          <Route path="clean" element={<CleanMainPage />}></Route>
+          <Route path="clean">
+            <Route index element={<CleanMain />} />
+            <Route path="trash" element={<Trash />} />
+            <Route path="sewage" element={<Sewage />} />
+            <Route path="recycle-statistics" element={<RecycleStatistics />} />
+            <Route path="recycle" element={<Recycle />} />
+            <Route path="pet" element={<Pet />} />
+            <Route path="gn-recycle-center" element={<GNRecycleCenter />} />
+            <Route path="gn-env-re-center" element={<GNEnvReCenter />} />
+            <Route path="gn-request" element={<GeneralRequest />} />
+            <Route path="biz-trash" element={<BizTrash />} />
+            <Route path="coffee-ground" element={<CoffeeGround />} />
+            <Route path="gn-civil" element={<GenCivilComplaint />} />
+          </Route>
         </Route>
       </Routes>
     </Suspense>
---
+++ src/pages/CleanPage/CleanMain.tsx
@@ -0,0 +1,5 @@
+const CleanMain = () => {
+  return <div></div>;
+};
+
+export default CleanMain;
